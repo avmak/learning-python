2ef1d152e88dca00fd946de605a0f6f7cf963b8d
Alexey <al3x.mak.89@gmail.com>
2016-02-13 11:43:03 +0300

    The arguments are checked for type (3.py)

--- part4/3.py
+++ part4/3.py
@@ -3,11 +3,12 @@
 def adder(*pargs):
 	summ = pargs[0]													# выделение первого передаваемого аргумента переменной
 
-	if pargs[1:] and (type(pargs[0]) != type(pargs[1])):			# если передаваемых аргументов больше одного, и их типы отличаются, вывести сообщение и выйти из функции
-		return print('The types of arguments are different!')
-
 	if type(summ) == type({}):										# если передаваемый аргумент словарь, то вывести сообщение и выйти из функции
 		return print('This is a dictionary!!!')
+
+	for i in pargs[1:]:												# если передаваемых аргументов больше одного, и их типы отличаются, вывести сообщение и выйти из функции
+		if type(summ) != type(i):
+			return print('The types of arguments are different!')
 		
 	for i in pargs[1:]:												# цикл сложения значений передаваемых аргументов
 		summ += i
