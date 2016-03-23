491dc25be69f4c121ef6c17ed080616d68a648e6
Alexey <al3x.mak.89@gmail.com>
2016-03-23 17:22:34 +0300

    The class wraps lists with overloaded operators (mylist.py / 2 task / part6)

--- part6/mylist.py
+++ part6/mylist.py
@@ -1,1 +1,76 @@
+class MyList:
+	def __init__(self, listin = []):
+		print("__init__ is performed")
+		self.data = []
+		if isinstance(listin, MyList):
+			listin = listin.data[:]
+		for x in listin:
+			self.data.append(x)
+	
+	def __add__(self, otherlist):
+		print("__add__ is performed")
+		if isinstance(otherlist, MyList):
+			otherlist = otherlist.data
+		return MyList(self.data + otherlist)
+	def __radd__(self, otherlist):
+		print("__radd__ is performed")
+		return MyList(otherlist + self.data)
+	def __iadd__(self, otherlist):
+		print("__iadd__ is performed")
+		self.data += otherlist
+		return self
+	
+	def __getitem__(self, index):
+		print("get item: ", index)
+		return self.data[index]
 
+	def __iter__(self):
+		print("__iter__ is performed")
+		self.ix = 0
+		return self
+	def __next__(self):
+		if self.ix == len(self.data):
+			raise StopIteration
+		item = self.data[self.ix]
+		self.ix += 1
+		return item
+
+	def __len__(self):
+		print("__len__ is performed")
+		return len(self.data)
+
+	def append(self, val):
+		print("append is performed")
+		self.data.append(val)
+
+	def sort(self):
+		print("sort is performed")
+		self.data.sort()
+
+	def __getattr__(self, nameattr):
+		print("__getattr__ is performed")
+		return getattr(self.data, nameattr)
+
+	def __repr__(self):
+		print("__repr__ is performed")
+		return repr(self.data)
+
+if __name__ == "__main__":
+	x = MyList('123')
+	y = MyList(x)
+	print(y)
+	print(y[2])
+	print(y[0:2])
+	print(y + ['2'])
+	print(['2'] + y)
+	print(x + y)
+	y += ['2']
+	print(y)
+	for ix in y:
+		print(ix, end = ' ')
+	y.append('2')
+	print(y)
+	y.sort()
+	print(y)
+	y.reverse()
+	print(y)
