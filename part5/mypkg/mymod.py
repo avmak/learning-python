8def7414f9844df7aeb921da320f487dedd0dfab
Alexey <al3x.mak.89@gmail.com>
2016-03-24 15:28:59 +0300

    change comments

--- part5/mypkg/mymod.py
+++ part5/mypkg/mymod.py
@@ -1,17 +1,17 @@
-def countLines(name):													#Function counts the line in the file
+def countLines(name):							#Function counts the line in the file
 	return len(open(name).readlines())
 
-def countChars(name):													#Function counts the characters in the file
+def countChars(name):							#Function counts the characters in the file
 	return len(open(name).read())
 
-def test(name):															#Test function
+def test(name):								#Test function
 	return countLines(name), countChars(name)
 
 #Option self-test modules
 if __name__ == '__main__': print(test('myclient.py'))
 
-#if __name__ == '__main__': print(test(input('Enter file name: ')))		#Using the user input
+#if __name__ == '__main__': print(test(input('Enter file name: ')))	#Using the user input
 
-#if __name__ == '__main__':												#Using the command line arguments
+#if __name__ == '__main__':						#Using the command line arguments
 #	import sys
 #	print(test(sys.argv[1]))
